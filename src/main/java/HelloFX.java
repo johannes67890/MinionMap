1a907aef6a46e11fc60039a2fe6dfcb19d622a81
Spurberino <elia07891@gmail.com>
2024-03-07 11:48:30 +0100

    Added handin1 proof of concept to start

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -8,10 +11,21 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        var input = new TextField();
+        var output = new TextArea();
+        var pane = new BorderPane();
+
+        pane.setTop(input);
+        pane.setCenter(output);
+
+        input.setOnAction(e->{
+            var a = Address.parse(input.getText());
+            output.setText(a.toString());
+        });
+
+        var scene = new Scene(pane);
+
+        stage.setTitle("Address Parsing");
         stage.setScene(scene);
         stage.show();
     }
