77e7d17ea7dd9d21cfaf7f39d423a017f209f11d
Caniuss <johannes@orager.dk>
2024-04-02 13:03:32 +0200

    minor change

--- src/main/java/GUI/Controller.java
+++ src/main/java/GUI/Controller.java
@@ -18,12 +18,6 @@ public class Controller {
         
         mainView.canvas.setOnScroll(event -> {
 
-<<<<<<< HEAD
-=======
-
-            //System.out.println("ZOOMING IN CONTROLLER");
-
->>>>>>> 72c9a6e5d383d6c40a5502023a4873bef52eac75
             mainView.getDrawingMap().zoom(Math.pow(zoomMultiplier,event.getDeltaY()), event.getX(), event.getY());
             
         });
