cd047eda7dea0283ef4327e5a719dbdfdf1fac1e
Johannes <johannes@orager.dk>
2024-04-02 21:49:09 +0200

    init

--- src/main/java/pathfinding/Dijsktra.java
+++ src/main/java/pathfinding/Dijsktra.java
@@ -1,5 +1,14 @@
 package pathfinding;
 
+import parser.TagNode;
+
 public class Dijsktra {
-    
+    Digraph G;
+
+    public Dijsktra(Digraph G, TagNode start, TagNode end) {
+        this.G = G;
+        
+        
+
+    }
 }
