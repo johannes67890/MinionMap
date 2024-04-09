021bbc1284baf133e38a9ff9a8df1183cf010cd0
Marius Cornelius Wisniewski Larsen <54457839+MessiGames30@users.noreply.github.com>
2024-04-09 13:14:36 +0200

    Further functionality in Tree class

--- src/main/java/util/Tree.java
+++ src/main/java/util/Tree.java
@@ -1,6 +1,7 @@
 package util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
@@ -13,19 +14,43 @@ public class Tree {
     public ArrayList<TagNode> nodes;
     TagNode node;
 
+    KdTree kdtree = new KdTree();
+
+    private HashSet<Point2D> points = new HashSet<Point2D>();
+
     public Tree(ArrayList<TagNode> nodes){
         this.nodes = nodes;
         
-        KdTree kdtree = new KdTree();
-
         for (TagNode node : nodes){
-            kdtree.insert(new Point2D(node.getLon(), node.getLat()));
+            this.kdtree.insert(new Point2D(node.getLon(), node.getLat()));
         }
     }
+
+    
     
     public void setScreenBounds(int width, int height) {
         
         RectHV rect = new RectHV(0, 0, width, height);
+        setNodesInBounds(rect);
     }
+
+    /**
+     * Sets nodes in the given bounds
+     * @param rect screenbounds
+     */
+    private void setNodesInBounds(RectHV rect) {
+        HashSet<Point2D> points = new HashSet<Point2D>();
+        this.kdtree.range(rect).forEach(point -> {points.add(point);});
+
+    }
+
+    /**
+     * @return HashSet<Point2D> of nodes in the given bounds
+     */
+    public HashSet<Point2D> getNodesInBounds() {
+        return this.points;
+    }
+
+
 
 }
