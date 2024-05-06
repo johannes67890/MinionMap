61bc0d298575a60a6403a684638b86fa430a6463
Marius Cornelius Wisniewski Larsen <54457839+MessiGames30@users.noreply.github.com>
2024-05-06 21:47:08 +0200

    Test made for model class

--- src/test/java/parser/ModelTest.java
+++ src/test/java/parser/ModelTest.java
@@ -1,23 +1,52 @@
 package parser;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.reporting.shadow.org.opentest4j.reporting.events.core.Tag;
+
+import util.FileDistributer;
+import util.FileHandler;
+
 
 import java.io.File;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
 import util.FileDistributer;
 import util.FileHandler;
 
 public class ModelTest {
 
-    @BeforeEach
-    void setUp() {
+    
+    @Test
+    public void testGetNodes() {
+
+        new XMLReader(FileHandler.getFileInputStream(new File(FileDistributer.testMap.getFilePath())));
+        int size = XMLReader.getNodes().valueCollection().size();
+
         Model.updateModelValues(FileHandler.getFileInputStream(new File(FileDistributer.testMap.getFilePath())));
         assertNotNull(Model.getInstanceModel());
         assertDoesNotThrow(() -> Model.getInstanceModel());
+
+        assertEquals(size, Model.getInstanceModel().getNodes().valueCollection().size());
     }
+
+    @Test
+    public void testGetBounds(){
+        new XMLReader(FileHandler.getFileInputStream(new File(FileDistributer.testMap.getFilePath())));
+        TagBound bound = XMLReader.getBound();
+
+        Model.updateModelValues(FileHandler.getFileInputStream(new File(FileDistributer.testMap.getFilePath())));
+        assertNotNull(Model.getInstanceModel());
+        assertDoesNotThrow(() -> Model.getInstanceModel());
+        
+        assertTrue(bound.toString().contains(Model.getInstanceModel().getBound().toString()));
+    }
+
+
 }
